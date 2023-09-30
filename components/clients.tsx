321c474009c94f2e440421e607d562bf652c5f37
moonchoudesign <moonchoudesign@gmail.com>
2023-09-30 12:39:26 +0800

    solved clients issue

--- components/clients.tsx
+++ components/clients.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 import { FadeIn, FadeInStagger } from "./FadeIn";
 import logoAmazon from "../public/assets/clients/amazon.png";
@@ -15,39 +15,39 @@ const clients = [
   ["Terra", logoTerra],
   ["Tiktok", logoTiktok],
   ["Proya", logoProya],
-]
+];
 
-export function Clients() {
-  return ( 
-    <div className='mb-16'>
-        <FadeInStagger className="mt-16" faste>
-          <div className=" relative m-auto items-center  overflow-hidden  before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,black_0%,rgba(0,0,0,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,black_0%,rgba(0,0,0,0)_100%)] after:content-['']">
-            <div className="animate-infinite-slider flex w-[calc(250px*10)]">
-              {clients.map(([client, logo]) => (
-                <div
-                  className="slide mr-32 w-[120px] flex items-center justify-center "
-                  key={client}
-                >
-                  <FadeIn>
-                    <Image src={logo} alt={client} unoptimized />
-                  </FadeIn>
-                </div>
-              ))}
-              {clients.map(([client, logo]) => (
-                <div
-                  className="slide mr-32 w-[120px] flex items-center justify-center "
-                  key={client}
-                >
-                  <FadeIn>
-                    <Image src={logo} alt={client} unoptimized />
-                  </FadeIn>
-                </div>
-              ))}
-            </div>
+export const Clients = () => {
+  return (
+    <div className="mb-16">
+      <FadeInStagger className="mt-16" faste>
+        <div className="relative m-auto items-center  overflow-hidden  before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,black_0%,rgba(0,0,0,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,black_0%,rgba(0,0,0,0)_100%)] after:content-['']">
+          <div className="animate-infinite-slider flex w-[calc(250px*10)]">
+            {clients.map(([client, logo], index) => (
+              <div
+                className="slide mr-32 w-[120px] flex items-center justify-center"
+                key={`${client}-${index}`}
+              >
+                <FadeIn>
+                  <Image src={logo} alt={client} unoptimized />
+                </FadeIn>
+              </div>
+            ))}
+            {clients.map(([client, logo], index) => (
+              <div
+                className="slide mr-32 w-[120px] flex items-center justify-center"
+                key={`${client}-${index}`}
+              >
+                <FadeIn>
+                  <Image src={logo} alt={client} unoptimized />
+                </FadeIn>
+              </div>
+            ))}
           </div>
-        </FadeInStagger>
+        </div>
+      </FadeInStagger>
     </div>
   );
 };
 
-export default Clients
+export default Clients;
