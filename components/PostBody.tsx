c230a3b1e0c24406e1b3d6d4380eafb8f1070249
moonchou <moonchoudesign@gmail.com>
2024-02-29 20:08:38 +0800

    feat: update favicon

--- components/PostBody.tsx
+++ components/PostBody.tsx
@@ -9,7 +9,7 @@ const PostBody = ({ content }: Props) => {
   return (
     <FadeIn>
     <div className="grid mb-[340px] text-white  pb-64 md:mb-[1000px] rounded-b-lg">
-    <div className="mx-auto box-border  ">
+    <div className="mx-auto box-border ">
       <div
         className={markdownStyles['markdown']}
         dangerouslySetInnerHTML={{ __html: content }}
